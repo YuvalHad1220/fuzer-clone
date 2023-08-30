ad9a43cb9625269d8cfba04a0a6bcf64299ebee8
MilkaPeeka <35yuval2@gmail.com>
2023-08-30 22:25:03 +0300

    correct cursor location

--- src/General/MarkdownEditor.tsx
+++ src/General/MarkdownEditor.tsx
@@ -6,32 +6,47 @@ import MuiMarkdown from "mui-markdown";
 const markdownWithType = (type: string, text: string) => {
     switch (type) {
         case "bold":
-            return `**${text}**`;
+            const boldText = `**${text}**`;
+            const boldCursorPosition = 2 + text.length;
+            return { newText: boldText, newCursorPosition: boldCursorPosition };
         case "italic":
-            return `*${text}*`;
-        // case "underline":
-        //     return `_${text}_`;
+            const italicText = `*${text}*`;
+            const italicCursorPosition = 1 + text.length;
+            return { newText: italicText, newCursorPosition: italicCursorPosition };
+        case "link":
+            const linkText = `[${text.length > 0 ? text : "הכנס תיאור קישור"}](הכנס כתובת קישור)`;
+            const linkCursorPosition = linkText.length;
+            return { newText: linkText, newCursorPosition: linkCursorPosition };
+        case "image":
+            const imageText = `${text}[](הכנס כתובת תמונה)`;
+            const imageCursorPosition = imageText.length;
+            return { newText: imageText, newCursorPosition: imageCursorPosition };
         case "header1":
-            return `#### ${text}`;
+            const header1Text = `#### ${text}`;
+            const header1CursorPosition = 5 + text.length;
+            return { newText: header1Text, newCursorPosition: header1CursorPosition };
         case "header2":
-            return `##### ${text}`;
+            const header2Text = `##### ${text}`;
+            const header2CursorPosition = 6 + text.length;
+            return { newText: header2Text, newCursorPosition: header2CursorPosition };
         case "header3":
-            return `###### ${text}`;
-        case "link": 
-            return `[${text.length > 0 ? text : "הכנס תיאור קישור"}](הכנס כתובת קישור)`;
-        case "image": 
-            return text + "[](הכנס כתובת תמונה)";
+            const header3Text = `###### ${text}`;
+            const header3CursorPosition = 7 + text.length;
+            return { newText: header3Text, newCursorPosition: header3CursorPosition };
+           
         default:
-            return text;
+            return { newText: text, newCursorPosition: text.length };
     }
 };
 
 
 const MarkdownEditor: React.FC = () => {
+
     const textRef = useRef<HTMLTextAreaElement>();
     const [markdownText, setMarkdownText] = useState<string>("");
     const [newCursorLocation, setNewCursorLocation] = useState<number>(0);
 
+    // IDEA: replace less than 4 # with 4 #
     const onTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setMarkdownText(e.target.value);
     };
@@ -40,17 +55,15 @@ const MarkdownEditor: React.FC = () => {
         const textarea = textRef.current;
             if (!textarea) return;
 
-        // indexes of part of text to make bold (if no text was selected but just a cursor location - start and end will share index)
         const start = textarea.selectionStart;
         const end = textarea.selectionEnd;
-        
         const selectedText = markdownText.substring(start, end);
 
-        // just marking the selected text as bold
-        const newText = markdownText.substring(0, start) + markdownWithType(type, selectedText) + markdownText.substring(end);
+        const {newText, newCursorPosition} = markdownWithType(type, selectedText);
+        const finalText = markdownText.substring(0, start) + newText + markdownText.substring(end);
 
-        setMarkdownText(newText);
-        setNewCursorLocation(start + 2 + selectedText.length);
+        setMarkdownText(finalText);
+        setNewCursorLocation(start + newCursorPosition);
     };
 
 
@@ -66,12 +79,11 @@ const MarkdownEditor: React.FC = () => {
     return (
         <Paper>
             <ButtonGroup fullWidth>
-                <Button onClick={() => insert("bold")}>בולט</Button>
-                {/* <Button onClick={() => insert("underline")}>קו תחתון</Button> */}
-                <Button onClick={() => insert("italic")}>הטייה</Button>
-                <Button onClick={() => insert("header1")}>כותרת 1</Button>
+            <Button onClick={() => insert("header1")}>כותרת 1</Button>
                 <Button onClick={() => insert("header2")}>כותרת 2</Button>
                 <Button onClick={() => insert("header3")}>כותרת 3</Button>
+                <Button onClick={() => insert("bold")}>בולט</Button>
+                <Button onClick={() => insert("italic")}>הטייה</Button>
                 <Button onClick={() => insert("image")}>הוספת תמונה</Button>
                 <Button onClick={() => insert("link")}>הוספת קישור</Button>
                 <Button>אימוגים</Button>
