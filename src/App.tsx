89bebe9f96583bacb4c37c7f8dcd1cec71b67e77
Yuval Hadar <35yuval2@gmail.com>
2023-08-30 11:28:41 +0300

    working scrollable bar, need to tidy up the code and add support for literally imdb

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Box } from "@mui/system"
 import CompactTable from "./Homepage/CompactTable"
+import IMDBBar from "./Homepage/IMDBBar"
 
 function App() {
 
   return (
-    <Box display="flex" flexDirection="row" gap={2} padding={2}>
-      <CompactTable />
-      <CompactTable />
+    <Box display="flex" flexDirection="column" gap={2} padding={2}>
+          <Box display="flex" flexDirection="row" gap={2} padding={2}>
+            <CompactTable />
+            <CompactTable />
+          </Box>
+
+
+      <IMDBBar/>
     </Box>
+
   )
 }
 
--- src/Homepage/IMDBBar.tsx
+++ src/Homepage/IMDBBar.tsx
@@ -1,4 +1,51 @@
-const IMDBBar = () => {
+import { Box, Fab, Paper } from "@mui/material";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { useRef } from "react";
 
+const scrollSteps = 10;
+const scrollSpeed = 70;
+
+const IMDBBar: React.FC = () => {
+    const scrollableBox = useRef<HTMLElement>();
+
+    let scrollInterval: NodeJS.Timeout;
+
+    const itemList = Array.from({ length: 20 }, (_, index) => `item ${index + 1}`);
+
+    const scroll = (dir: string) => {
+        scrollInterval = setInterval(() => {
+            if (!scrollableBox || !scrollableBox.current)
+            return;
+            if (dir === "left")
+                scrollableBox.current.scrollLeft -= scrollSteps;
+
+            else if (dir === "right")
+                scrollableBox.current.scrollLeft += scrollSteps;
+
+        }, scrollSpeed)
+
+    };
+
+    const stopScroll = () => {
+        clearInterval(scrollInterval)
+    }
+
+        return (
+            <>
+            <Box display="flex" flexDirection="row" justifyContent="space-between" position="relative" top="100px" zIndex={2} >
+                <Fab size="small" color="primary" onMouseEnter={() => scroll("right")} onMouseLeave={stopScroll} aria-label="גלול ימינה"><ChevronRightIcon/></Fab>
+                <Fab size="small" color="primary" onMouseEnter={() => scroll("left")} onMouseLeave={stopScroll} aria-label="גלול שמאלה"><ChevronLeftIcon/></Fab>
+            </Box>
+            <Paper>
+                <Box ref={scrollableBox} display="flex" flexDirection="row" gap={6} sx={{overflowX: "hidden", scrollBehavior:"smooth"}}>
+                    {itemList.map((item, i) => <h1 key={i}>{item}</h1>)}
+                </Box>
+            </Paper>
+            </>
+
+
+    );
 };
 
+export default IMDBBar;
